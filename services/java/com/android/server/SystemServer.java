28aed42dde3ddbc4698ff9932c7ae064c117aa55
Erfan Abdi <erfangplus@gmail.com>
2022-06-12 18:27:00 +0430

    frameworks: Import Lineage GlobalActions service
    Change-Id: I4a0e5823b1ca48e235ddd4add95a6e342e754857
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -317,6 +317,7 @@ import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
+import com.android.server.voltage.LineageGlobalActionsService;
 
 import dalvik.system.VMRuntime;
 
@@ -1875,6 +1876,10 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(TradeInModeService.class);
                 t.traceEnd();
             }
+
+            t.traceBegin("LineageGlobalActionsService");
+            mSystemServiceManager.startService(LineageGlobalActionsService.class);
+            t.traceEnd();
 
         } catch (Throwable e) {
             Slog.e("System", "******************************************");
