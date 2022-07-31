7c42e01bf7751aa07fb213dd1092dd38646cb28b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 15:29:26 +0300

    gosps: support for per-app GrapheneOS-specific persistent state
    Squashed with 0a99fc28522c93149f6d4d4e611ffc1d78a4d7e1 by quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -87,6 +87,7 @@ import android.content.pm.DataLoaderType;
 import android.content.pm.FallbackCategoryProvider;
 import android.content.pm.FeatureInfo;
 import android.content.pm.Flags;
+import android.content.pm.GosPackageState;
 import android.content.pm.IDexModuleRegisterCallback;
 import android.content.pm.IOnChecksumsReadyListener;
 import android.content.pm.IPackageDataObserver;
@@ -4372,6 +4373,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         if (dexUseManager != null) {
             dexUseManager.systemReady();
         }
+
+        GosPackageStatePmHooks.init(this);
     }
 
     //TODO: b/111402650
@@ -4827,6 +4830,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             mHandler.post(new Runnable() {
                 public void run() {
                     mHandler.removeCallbacks(this);
+
+                    GosPackageStatePmHooks.onClearApplicationUserData(
+                            PackageManagerService.this, packageName, userId);
+
                     final boolean succeeded;
                     try (PackageFreezer freezer = freezePackage(packageName, UserHandle.USER_ALL,
                             "clearApplicationUserData",
@@ -6727,6 +6734,22 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         public Bundle getExtraAppBindArgs(String packageName) {
             return PackageManagerHooks.getExtraAppBindArgs(PackageManagerService.this, packageName);
         }
+
+        @Override
+        public GosPackageState getGosPackageState(@NonNull String packageName, int userId) {
+            int callingUid = Binder.getCallingUid();
+            int callingPid = Binder.getCallingPid();
+            return GosPackageStatePmHooks.getFiltered(PackageManagerService.this, callingUid, callingPid, packageName, userId);
+        }
+
+        @Override
+        public boolean setGosPackageState(@NonNull String packageName, int userId,
+                                                  @NonNull GosPackageState updatedPs, int editorFlags) {
+            int callingUid = Binder.getCallingUid();
+            int callingPid = Binder.getCallingPid();
+            return GosPackageStatePmHooks.set(PackageManagerService.this, callingUid, callingPid, packageName, userId,
+                    updatedPs, editorFlags);
+        }
     }
 
     private class PackageManagerInternalImpl extends PackageManagerInternalBase {
@@ -7309,6 +7332,12 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         public boolean isUpgradingFromLowerThan(int sdkVersion) {
             final boolean isUpgrading = mPriorSdkVersion != -1;
             return isUpgrading && mPriorSdkVersion < sdkVersion;
+        }
+
+        @NonNull
+        @Override
+        public GosPackageState getGosPackageState(String packageName, int userId) {
+            return GosPackageStatePmHooks.getUnfiltered(PackageManagerService.this, packageName, userId);
         }
     }
 
