3485e6a9898ce6f01c778912bfc80efc1d8e4fda
Pranav Vashi <neobuddy89@gmail.com>
2022-12-29 22:34:58 +0530

    KeyguardIndication: Fix glitchy charging info on AOD
    * This is second fix following https://github.com/crdroidandroid/android_frameworks_base/commit/b0c0883b4fc9bd06e6d13aca427cfbf4f13e5f38
    * Seems to be AOSP bug. :)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1167,7 +1167,7 @@ public class KeyguardIndicationController {
 
                 mTopIndicationView.switchIndication(newIndication,
                         builder.build(),
-                        true, () -> mWakeLock.setAcquired(false));
+                        animate, () -> mWakeLock.setAcquired(false));
             }
             return;
         }
