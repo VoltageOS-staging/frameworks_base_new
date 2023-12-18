cc6d2390501316242fa0b3ce176b3da40bb907fd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 14:56:03 +0200

    add method for updating cached package permission state
    Package permission state is updated automatically for all packages after events that might impact
    it, e.g. after package install or uninstall, after storage volume mount, after OS update etc.
    
    On GrapheneOS, per-package permission policy can be changed via GosPackageState packageFlags.
    This new method is needed for updating the cached permission state manually after packageFlags
    change.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/permission/PermissionManager.java
+++ core/java/android/permission/PermissionManager.java
@@ -2182,4 +2182,12 @@ public final class PermissionManager {
                     + '}';
         }
     }
+
+    public void updatePermissionState(@NonNull String packageName, int userId) {
+        try {
+            mPermissionManager.updatePermissionState(packageName, userId);
+        } catch (RemoteException e) {
+            e.rethrowFromSystemServer();
+        }
+    }
 }
