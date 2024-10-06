0af9ba0434238feb31e0fa5c250fe786f5223c34
Pranav Vashi <neobuddy89@gmail.com>
2024-10-06 06:37:58 +0000

    SystemUI: Disable smartspace weather by default
    * Likely gapps are settings this overlay config to 1.
      So, hardcore disable by default
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceController.kt
@@ -319,8 +319,11 @@ constructor(
     val isWeatherEnabled: Boolean
         get() {
             val showWeather =
-                secureSettings.getIntForUser(LOCK_SCREEN_WEATHER_ENABLED, 1, userTracker.userId) ==
-                    1
+                secureSettings.getIntForUser(
+                    LOCK_SCREEN_WEATHER_ENABLED,
+                    0,
+                    userTracker.userId,
+                ) == 1
             return showWeather
         }
 
