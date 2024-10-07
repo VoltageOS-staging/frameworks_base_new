4ddecc3de6a17c21f1b52c7170fb76a98180ad97
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-07 18:59:24 +0300

    include path of ANR stack traces file in ApplicationErrorReport
    ANR stack traces file contains stack traces of all app's threads and of all threads of relevant or
    possibly relevant system processes, such as system_server.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/ApplicationErrorReport.java
+++ core/java/android/app/ApplicationErrorReport.java
@@ -16,6 +16,7 @@
 
 package android.app;
 
+import android.annotation.Nullable;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -574,6 +575,10 @@ public class ApplicationErrorReport implements Parcelable {
          */
         public String info;
 
+        /** @hide */
+        @Nullable
+        public String tracesFilePath;
+
         /**
          * Create an uninitialized instance of AnrInfo.
          */
@@ -587,6 +592,7 @@ public class ApplicationErrorReport implements Parcelable {
             activity = in.readString();
             cause = in.readString();
             info = in.readString();
+            tracesFilePath = in.readString();
         }
 
         /**
@@ -596,6 +602,7 @@ public class ApplicationErrorReport implements Parcelable {
             dest.writeString(activity);
             dest.writeString(cause);
             dest.writeString(info);
+            dest.writeString(tracesFilePath);
         }
 
         /**
