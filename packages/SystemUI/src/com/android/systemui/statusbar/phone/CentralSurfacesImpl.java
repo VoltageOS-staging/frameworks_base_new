11805b470fa3385b926faee75a3662fa73464888
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-28 22:42:48 +0800

    Add ScrimUtils and scrim-related feature fixes
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2667,8 +2667,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
-            mMediaArtUtils.updateMediaArtVisibility();
-            mWallpaperDepthUtils.updateDepthWallpaperVisibility();
             if (Settings.System.getIntForUser(mContext.getContentResolver(),
                                               Settings.System.ARCANE_IDLE_MANAGER, 1,
                                               mLockscreenUserManager.getCurrentUserId()) == 1) {
@@ -2747,6 +2745,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            com.android.systemui.util.ScrimUtils.getInstance(mContext).onScreenStateChange();
             if (Settings.System.getIntForUser(mContext.getContentResolver(),
                                               Settings.System.ARCANE_IDLE_MANAGER, 1,
                                               mLockscreenUserManager.getCurrentUserId()) == 1) {
