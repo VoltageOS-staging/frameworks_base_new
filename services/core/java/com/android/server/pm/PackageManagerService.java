e455189df1cf40e58497be8a4e9f9dc7027deb11
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-02-26 12:14:37 +0200

    add getter for PackageManagerService.mContext
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8250,4 +8250,9 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         return UserHandle.isSameApp(uid, Process.SYSTEM_UID)
                 || UserHandle.isSameApp(uid, Process.PHONE_UID);
     }
+
+    @NonNull
+    public Context getContext() {
+        return mContext;
+    }
 }
