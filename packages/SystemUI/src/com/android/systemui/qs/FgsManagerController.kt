VoltageOS/frameworks_base

SystemUI: Allow to exlude apps from running apps notification
Signed-off-by: tejasvp25 <tejasvp25@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/FgsManagerController.kt
@@ -223,6 +223,10 @@ class FgsManagerControllerImpl @Inject constructor(
 
     private val userVisibleJobObserver = UserVisibleJobObserver()
 
+    @GuardedBy("lock")
+    private val excludedApps: Array<String> = context.resources.getStringArray(R.array.excluded_apps)
+
+
     override fun init() {
         synchronized(lock) {
             if (initialized) {
@@ -295,7 +299,6 @@ class FgsManagerControllerImpl @Inject constructor(
                 executor = mainExecutor,
                 flags = Context.RECEIVER_NOT_EXPORTED
             )
-
             initialized = true
         }
     }
@@ -610,7 +613,7 @@ class FgsManagerControllerImpl @Inject constructor(
         ) {
             synchronized(lock) {
                 val userPackageKey = UserPackage(userId, packageName)
-                if (isForeground) {
+                if (isForeground && !excludedApps.contains(packageName)) {
                     runningTaskIdentifiers
                             .getOrPut(userPackageKey) { StartTimeAndIdentifiers(systemClock) }
                             .addFgsToken(token)
@@ -637,7 +640,7 @@ class FgsManagerControllerImpl @Inject constructor(
         ) {
             synchronized(lock) {
                 val userPackageKey = UserPackage(summary.sourceUserId, summary.sourcePackageName)
-                if (isRunning) {
+                if (isRunning && !excludedApps.contains(summary.sourcePackageName)) {
                     runningTaskIdentifiers
                             .getOrPut(userPackageKey) { StartTimeAndIdentifiers(systemClock) }
                             .addJobSummary(summary)

VoltageOS-staging/frameworks_base_new

packages/SystemUI/src/com/android/systemui/qs/FgsManagerController.kt
@@ -141,6 +141,7 @@ interface FgsManagerController {
 class FgsManagerControllerImpl
 @Inject
 constructor(
+    private val context: Context,
     @ShadeDisplayAware private val resources: Resources,
     @Main private val mainExecutor: Executor,
     @Background private val backgroundExecutor: Executor,
@@ -231,6 +232,9 @@ constructor(
         resources.getStringArray(com.android.internal.R.array.vendor_stoppable_fgs_system_apps)
     }
 
+    @GuardedBy("lock")
+    private val excludedApps: Array<String> = context.resources.getStringArray(R.array.excluded_apps)
+
     override fun init() {
         synchronized(lock) {
             if (initialized) {
@@ -318,7 +322,6 @@ constructor(
                 executor = mainExecutor,
                 flags = Context.RECEIVER_NOT_EXPORTED,
             )
-
             initialized = true
         }
     }
@@ -654,7 +657,7 @@ constructor(
         ) {
             synchronized(lock) {
                 val userPackageKey = UserPackage(userId, packageName)
-                if (isForeground) {
+                if (isForeground && !excludedApps.contains(packageName)) {
                     runningTaskIdentifiers
                         .getOrPut(userPackageKey) { StartTimeAndIdentifiers(systemClock) }
                         .addFgsToken(token)
@@ -686,7 +689,7 @@ constructor(
                         UserHandle.getUserId(summary.callingUid),
                         summary.callingPackageName,
                     )
-                if (isRunning) {
+                if (isRunning && !excludedApps.contains(summary.callingPackageName)) {
                     runningTaskIdentifiers
                         .getOrPut(userPackageKey) { StartTimeAndIdentifiers(systemClock) }
                         .addJobSummary(summary)