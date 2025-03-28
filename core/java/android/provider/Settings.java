00e823f13827bd8b4b95807f0cf82a0b90faf6fd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-28 19:11:30 +0200

    SettingsProvider: add support for further restriction of setting access
    By default, Settings.{Global,Secure,System} that aren't annotated with `@Readable` are
    readable only by preinstalled apps (with some exceptions, see enforceSettingReadable()).
    
    Settings.{Global,Secure} settings are writable only by apps that hold the privileged
    WRITE_SECURE_SETTINGS permission. Settings.System are also writable by apps that hold the
    WRITE_SETTINGS app-op permission (it's surfaced as "Modify system settings" in the UI).
    
    This commit adds `@Protected` setting annotation, which allows to further restrict settings access
    by specifying which system apps are allowed to read and/or write them.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -61,6 +61,7 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.database.SQLException;
+import android.ext.KnownSystemPackage;
 import android.location.ILocationManager;
 import android.location.LocationManager;
 import android.media.AudioManager;
@@ -3527,7 +3528,9 @@ public final class Settings {
             mAllFields = new ArraySet<>();
             mReadableFieldsWithMaxTargetSdk = new ArrayMap<>();
             getPublicSettingsForClass(callerClass, mAllFields, mReadableFields,
-                    mReadableFieldsWithMaxTargetSdk);
+                    mReadableFieldsWithMaxTargetSdk,
+                    // skip obtaining protectedSettings map, it's not needed for NameValueCache
+                    null);
         }
 
         // Returns last path component of the relevant Uri.
@@ -4085,9 +4088,26 @@ public final class Settings {
         int maxTargetSdk() default 0;
     }
 
+    @Target({ ElementType.FIELD })
+    @Retention(RetentionPolicy.RUNTIME)
+    private @interface Protected {
+        boolean restrictReads() default true;
+        // IDs of system packages that are allowed read-only access. Ignored if restrictReads is false.
+        @KnownSystemPackage.Enum int[] read() default {};
+        // IDs of system packages that are allowed read and write access
+        @KnownSystemPackage.Enum int[] readWrite();
+    }
+
+    /** @hide */
+    public record ProtectedSetting(String key,
+                                   boolean restrictReads,
+                                   @KnownSystemPackage.Enum int[] readableBy,
+                                   @KnownSystemPackage.Enum int[] readWritableBy) {}
+
     private static <T extends NameValueTable> void getPublicSettingsForClass(
             Class<T> callerClass, Set<String> allKeys, Set<String> readableKeys,
-            ArrayMap<String, Integer> keysWithMaxTargetSdk) {
+            ArrayMap<String, Integer> keysWithMaxTargetSdk,
+            @Nullable ArrayMap<String, ProtectedSetting> protectedSettings) {
         final Field[] allFields = callerClass.getDeclaredFields();
         try {
             for (int i = 0; i < allFields.length; i++) {
@@ -4099,15 +4119,24 @@ public final class Settings {
                 if (!value.getClass().equals(String.class)) {
                     continue;
                 }
-                allKeys.add((String) value);
+                final String key = (String) value;
+                allKeys.add(key);
                 final Readable annotation = field.getAnnotation(Readable.class);
 
                 if (annotation != null) {
-                    final String key = (String) value;
                     final int maxTargetSdk = annotation.maxTargetSdk();
                     readableKeys.add(key);
                     if (maxTargetSdk != 0) {
                         keysWithMaxTargetSdk.put(key, maxTargetSdk);
+                    }
+                }
+
+                if (protectedSettings != null) {
+                    final Protected anno = field.getAnnotation(Protected.class);
+                    if (anno != null) {
+                        var setting = new ProtectedSetting(key, anno.restrictReads(),
+                                anno.read(), anno.readWrite());
+                        protectedSettings.put(key, setting);
                     }
                 }
             }
@@ -4332,9 +4361,10 @@ public final class Settings {
 
         /** @hide */
         public static void getPublicSettings(Set<String> allKeys, Set<String> readableKeys,
-                ArrayMap<String, Integer> readableKeysWithMaxTargetSdk) {
+                ArrayMap<String, Integer> readableKeysWithMaxTargetSdk,
+                ArrayMap<String, ProtectedSetting> protectedSettings) {
             getPublicSettingsForClass(System.class, allKeys, readableKeys,
-                    readableKeysWithMaxTargetSdk);
+                    readableKeysWithMaxTargetSdk, protectedSettings);
         }
 
         /**
@@ -7937,9 +7967,10 @@ public final class Settings {
 
         /** @hide */
         public static void getPublicSettings(Set<String> allKeys, Set<String> readableKeys,
-                ArrayMap<String, Integer> readableKeysWithMaxTargetSdk) {
+                ArrayMap<String, Integer> readableKeysWithMaxTargetSdk,
+                ArrayMap<String, ProtectedSetting> protectedSettings) {
             getPublicSettingsForClass(Secure.class, allKeys, readableKeys,
-                    readableKeysWithMaxTargetSdk);
+                    readableKeysWithMaxTargetSdk, protectedSettings);
         }
 
         /**
@@ -19583,14 +19614,15 @@ public final class Settings {
 
         /** @hide */
         public static void getPublicSettings(Set<String> allKeys, Set<String> readableKeys,
-                ArrayMap<String, Integer> readableKeysWithMaxTargetSdk) {
+                ArrayMap<String, Integer> readableKeysWithMaxTargetSdk,
+                ArrayMap<String, ProtectedSetting> protectedSettings) {
             getPublicSettingsForClass(Global.class, allKeys, readableKeys,
-                    readableKeysWithMaxTargetSdk);
+                    readableKeysWithMaxTargetSdk, protectedSettings);
             // Add Global.Wearable keys on watches.
             if (ActivityThread.currentApplication().getApplicationContext().getPackageManager()
                     .hasSystemFeature(PackageManager.FEATURE_WATCH)) {
                 getPublicSettingsForClass(Global.Wearable.class, allKeys, readableKeys,
-                        readableKeysWithMaxTargetSdk);
+                        readableKeysWithMaxTargetSdk, protectedSettings);
             }
         }
 
