2e381e2436a88176565e930229e89976e3f67907
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-28 19:49:24 +0200

    fixup! add special runtime permission for other sensors
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7791,6 +7791,7 @@ public final class Settings {
         // ExtSettings BEGIN
 
         /** @hide */
+        @Protected(readWrite = KnownSystemPackage.SETTINGS)
         public static final String AUTO_GRANT_OTHER_SENSORS_PERMISSION = "auto_grant_OTHER_SENSORS_perm";
 
         /** @hide */
