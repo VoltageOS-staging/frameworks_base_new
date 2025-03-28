2f3874b3568918171073148ada157d6ba495a6f8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-28 19:31:03 +0200

    fixup! add auto-reboot feature
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14284,6 +14284,7 @@ public final class Settings {
                 "allow_automatic_pkg_hardening_config"; // historical name
 
         /** @hide */
+        @Protected(readWrite = KnownSystemPackage.SETTINGS)
         public static final String AUTO_REBOOT_TIMEOUT = "settings_reboot_after_timeout";
 
         /** @hide */
