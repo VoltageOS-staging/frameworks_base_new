3dec58f19c665947aa6302e411c48442f75e98fe
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-28 19:26:05 +0200

    fixup! Widevine provisioning setting
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14300,6 +14300,7 @@ public final class Settings {
         public static final String REMOTE_KEY_PROVISIONING_SERVER = "attest_remote_provisioner_server";
 
         /** @hide */
+        @Protected(restrictReads = false, readWrite = KnownSystemPackage.SETTINGS)
         public static final String WIDEVINE_PROVISIONING_SERVER = "widevine_provisioner_server";
 
         /** @hide */
