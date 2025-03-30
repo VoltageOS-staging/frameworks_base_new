ec2b4590fc136bb69c376add725ae84f96901cfc
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-30 18:08:09 +0300

    Revert "fixup! Disable "Add users when locked" settings on every boot"
    This reverts commit e697d1097849d1b87253a9087791e6ec9e61779a.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14454,7 +14454,6 @@ public final class Settings {
          * @hide
          */
         @Readable
-        @Protected(restrictReads = false, readWrite = {})
         public static final String ADD_USERS_WHEN_LOCKED = "add_users_when_locked";
 
         /**
