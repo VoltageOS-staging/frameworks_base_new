a4d347c4f74012cf8f14db8c318bd2767c34da95
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-03-30 09:49:24 +0000

    fixup! Disable "Add users when locked" settings on every boot
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14461,6 +14461,7 @@ public final class Settings {
          * @hide
          */
         @Readable
+        @Protected(restrictReads = false, readWrite = {})
         public static final String ADD_USERS_WHEN_LOCKED = "add_users_when_locked";
 
         /**
