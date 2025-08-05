custom-crdroid/android_frameworks_base

SystemUI: Fix smartspace layout
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/SmartspaceSection.kt
@@ -163,7 +163,7 @@ constructor(
                 connect(
                     sharedR.id.bc_smartspace_view,
                     ConstraintSet.TOP,
-                    sharedR.id.date_smartspace_view,
+                    R.id.lockscreen_clock_view,
                     ConstraintSet.BOTTOM
                 )
             }

VoltageOS-staging/frameworks_base_new

Change-Id: If3a3bd1908c566b8a2d9195fbac75b6ecbdf9a9b
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/SmartspaceSection.kt
@@ -228,7 +228,7 @@ constructor(
                     connect(
                         sharedR.id.bc_smartspace_view,
                         ConstraintSet.TOP,
-                        sharedR.id.date_smartspace_view,
+                        customR.id.lockscreen_clock_view,
                         ConstraintSet.BOTTOM,
                     )
                 }