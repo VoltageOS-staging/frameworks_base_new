1a684e3a3f90241dae7da85bd4339a086e128ec6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-20 12:16:38 +0800

    [AXIONUI] port tile specs from nos, multi-dpi scaling support
    Change-Id: I4c1e1d6b00153ee571ebd7eb5d1a7ef80ef239b3
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
@@ -1319,10 +1319,10 @@ fun QuickSettingsLayout(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             tiles()
-            spacerLayout(height = QuickSettingsShade.Dimensions.QsPadding)
+            spacerLayout(height = QuickSettingsShade.QSVerticalPadding() / 2)
             brightness()
             if (mediaVisible) {
-                spacerLayout(height = QuickSettingsShade.Dimensions.QsPadding)
+                spacerLayout(height = QuickSettingsShade.QSVerticalPadding() / 2)
             }
             media()
         }
