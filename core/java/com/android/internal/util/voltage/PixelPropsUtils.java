541d0afb7905741c91f6e7c9cb48981fe2ab41a6
Pranav Vashi <neobuddy89@gmail.com>
2025-10-22 21:33:06 +0900

    PixelPropsUtils: Fix failing strong integrity with PIF disabled
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/voltage/PixelPropsUtils.java
+++ core/java/com/android/internal/util/voltage/PixelPropsUtils.java
@@ -763,15 +763,16 @@ public final class PixelPropsUtils {
     }
 
     public static void onEngineGetCertificateChain() {
+        boolean isKeyBoxAvailable = KeyProviderManager.isKeyboxAvailable();
+
         boolean isPixelGmsEnabled = SystemProperties.getBoolean(SPOOF_GMS, true);
-        if (!isPixelGmsEnabled) {
+        if (!isKeyBoxAvailable && !isPixelGmsEnabled) {
             dlog("onEngineGetCertificateChain disabled by setting");
             return;
         }
 
         // If a keybox is found, don't block key attestation
-        if (SystemProperties.getBoolean(SPOOF_GMS_CERT_CHAIN, false)
-                && KeyProviderManager.isKeyboxAvailable()) {
+        if (isKeyBoxAvailable && SystemProperties.getBoolean(SPOOF_GMS_CERT_CHAIN, false)) {
             dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
             return;
         }
