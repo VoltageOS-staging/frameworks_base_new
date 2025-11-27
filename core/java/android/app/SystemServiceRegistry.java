DerpFest-CAF/platform_frameworks_base

SystemUI: Implement DisplayResolutionManager [1/2]
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
[nurkeinneid: move to fwb and drop sdk extensions/dependencies]
Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

core/java/android/app/SystemServiceRegistry.java
@@ -292,6 +292,8 @@ import com.android.internal.util.Preconditions;
 
 import org.derpfest.display.IRefreshRateManagerService;
 import org.derpfest.display.RefreshRateManager;
+import org.derpfest.view.DisplayResolutionManager;
+import org.derpfest.view.IDisplayResolutionManagerService;
 
 import java.time.InstantSource;
 import java.util.HashMap;
@@ -1826,6 +1828,15 @@ public final class SystemServiceRegistry {
                         return new IntrusionDetectionManager(service);
                     }
                 });
+
+        registerService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE, DisplayResolutionManager.class,
+                new CachedServiceFetcher<DisplayResolutionManager>() {
+            @Override
+            public DisplayResolutionManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE);
+                IDisplayResolutionManagerService service = IDisplayResolutionManagerService.Stub.asInterface(binder);
+                return new DisplayResolutionManager(ctx.getOuterContext(), service);
+            }});
 
         registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
                 new CachedServiceFetcher<RefreshRateManager>() {

VoltageOS-staging/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

core/java/android/app/SystemServiceRegistry.java
@@ -300,6 +300,8 @@ import com.android.internal.util.Preconditions;
 
 import com.voltage.display.IRefreshRateManagerService;
 import com.voltage.display.RefreshRateManager;
+import com.voltage.view.DisplayResolutionManager;
+import com.voltage.view.IDisplayResolutionManagerService;
 
 import java.util.Map;
 import java.util.Objects;
@@ -1875,6 +1877,15 @@ public final class SystemServiceRegistry {
                         }
                     });
         }
+
+        registerService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE, DisplayResolutionManager.class,
+                new CachedServiceFetcher<DisplayResolutionManager>() {
+            @Override
+            public DisplayResolutionManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE);
+                IDisplayResolutionManagerService service = IDisplayResolutionManagerService.Stub.asInterface(binder);
+                return new DisplayResolutionManager(ctx.getOuterContext(), service);
+            }});
 
         registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
                 new CachedServiceFetcher<RefreshRateManager>() {