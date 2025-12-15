LineageOS/android_lineage-sdk

health: Implement charge limit mode
Change-Id: I6d4f03a62555bd07ec70d23745506c7759b715ab

lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
@@ -30,6 +30,7 @@ import android.util.Log;
 import org.lineageos.platform.internal.R;
 import org.lineageos.platform.internal.health.ccprovider.ChargingControlProvider;
 import org.lineageos.platform.internal.health.ccprovider.Deadline;
+import org.lineageos.platform.internal.health.ccprovider.Limit;
 import org.lineageos.platform.internal.health.ccprovider.Toggle;
 
 import lineageos.providers.LineageSettings;
@@ -70,6 +71,9 @@ public class ChargingControlController extends LineageHealthFeature {
 
     // Current selected provider
     private ChargingControlProvider mCurrentProvider;
+    private Deadline mDeadline;
+    private Limit mLimit;
+    private Toggle mToggle;
 
     public ChargingControlController(Context context, Handler handler) {
         super(context, handler);
@@ -98,19 +102,16 @@ public class ChargingControlController extends LineageHealthFeature {
                 R.integer.config_defaultChargingControlLimit);
 
         // Set up charging control providers
-        mCurrentProvider = new Toggle(mChargingControl, mContext);
-        if (!mCurrentProvider.isSupported()) {
-            mCurrentProvider = null;
-        }
-
-        if (mCurrentProvider == null) {
-            mCurrentProvider = new Deadline(mChargingControl, mContext);
-            if (!mCurrentProvider.isSupported()) {
-                mCurrentProvider = null;
-            }
-        }
-
-        if (mCurrentProvider == null) {
+        mDeadline = new Deadline(mChargingControl, mContext);
+        mLimit = new Limit(mChargingControl, mContext);
+        mToggle = new Toggle(mChargingControl, mContext);
+        if (mLimit.isSupported()) {
+            mCurrentProvider = mLimit;
+        } else if (mToggle.isSupported()) {
+            mCurrentProvider = mToggle;
+        } else if (mDeadline.isSupported()) {
+            mCurrentProvider = mDeadline;
+        } else {
             Log.wtf(TAG, "No charging control provider is supported");
         }
     }
@@ -138,6 +139,23 @@ public class ChargingControlController extends LineageHealthFeature {
 
     public boolean setMode(int mode) {
         if (mode < MODE_NONE || mode > MODE_LIMIT) {
+            return false;
+        }
+
+        mCurrentProvider = null;
+        if (mode == MODE_LIMIT) {
+            if (mLimit.isSupported()) {
+                mCurrentProvider = mLimit;
+            } else if (mToggle.isSupported()) {
+                mCurrentProvider = mToggle;
+            }
+        } else if (mode == MODE_AUTO || mode == MODE_MANUAL) {
+            if (mDeadline.isSupported()) {
+                mCurrentProvider = mDeadline;
+            }
+        }
+
+        if (mCurrentProvider == null) {
             return false;
         }
 
@@ -288,6 +306,8 @@ public class ChargingControlController extends LineageHealthFeature {
     private void onPowerConnected() {
         if (mBattReceiver == null) {
             mBattReceiver = new LineageHealthBatteryBroadcastReceiver();
+        } else {
+            mContext.unregisterReceiver(mBattReceiver);
         }
         IntentFilter battFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         mContext.registerReceiver(mBattReceiver, battFilter);
@@ -296,6 +316,7 @@ public class ChargingControlController extends LineageHealthFeature {
     private void onPowerDisconnected() {
         if (mBattReceiver != null) {
             mContext.unregisterReceiver(mBattReceiver);
+            mBattReceiver = null;
         }
 
         // On disconnected, reset internal state

VoltageOS-staging/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

services/core/java/com/android/server/custom/health/ChargingControlController.java
@@ -39,6 +39,7 @@ import java.io.PrintWriter;
 
 import com.android.server.custom.health.ccprovider.ChargingControlProvider;
 import com.android.server.custom.health.ccprovider.Deadline;
+import com.android.server.custom.health.ccprovider.Limit;
 import com.android.server.custom.health.ccprovider.Toggle;
 
 import android.provider.Settings;
@@ -82,6 +83,9 @@ public class ChargingControlController extends LineageHealthFeature {
 
     // Current selected provider
     private ChargingControlProvider mCurrentProvider;
+    private Deadline mDeadline;
+    private Limit mLimit;
+    private Toggle mToggle;
 
     public ChargingControlController(Context context, Handler handler) {
         super(context, handler);
@@ -110,17 +114,16 @@ public class ChargingControlController extends LineageHealthFeature {
                 R.integer.config_defaultChargingControlLimit);
 
         // Set up charging control providers
-        mCurrentProvider = new Toggle(mChargingControl, mContext);
-        if (!mCurrentProvider.isSupported()) {
-            mCurrentProvider = null;
-        }
-        if (mCurrentProvider == null) {
-            mCurrentProvider = new Deadline(mChargingControl, mContext);
-            if (!mCurrentProvider.isSupported()) {
-                mCurrentProvider = null;
-            }
-        }
-        if (mCurrentProvider == null) {
+        mDeadline = new Deadline(mChargingControl, mContext);
+        mLimit = new Limit(mChargingControl, mContext);
+        mToggle = new Toggle(mChargingControl, mContext);
+        if (mLimit.isSupported()) {
+            mCurrentProvider = mLimit;
+        } else if (mToggle.isSupported()) {
+            mCurrentProvider = mToggle;
+        } else if (mDeadline.isSupported()) {
+            mCurrentProvider = mDeadline;
+        } else {
             Log.wtf(TAG, "No charging control provider is supported");
         }
     }
@@ -147,6 +150,22 @@ public class ChargingControlController extends LineageHealthFeature {
 
     public boolean setMode(int mode) {
         if (mode < MODE_NONE || mode > MODE_LIMIT) {
+            return false;
+        }
+
+        if (mode == MODE_LIMIT) {
+            if (mLimit.isSupported()) {
+                mCurrentProvider = mLimit;
+            } else if (mToggle.isSupported()) {
+                mCurrentProvider = mToggle;
+            }
+        } else if (mode == MODE_AUTO || mode == MODE_MANUAL) {
+            if (mDeadline.isSupported()) {
+                mCurrentProvider = mDeadline;
+            }
+        }
+
+        if (mCurrentProvider == null) {
             return false;
         }
 