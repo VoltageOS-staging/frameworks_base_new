514b73f4ca383c3a744e55e821933f0062c07517
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-01-02 19:59:51 +0000

    fixup! infrastructure for spoofing self permission checks
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/permission/PermissionManager.java
+++ core/java/android/permission/PermissionManager.java
@@ -2000,7 +2000,7 @@ public final class PermissionManager {
         }
 
         if (res != PERMISSION_GRANTED) {
-            if (pkgName.equals(ActivityThread.currentPackageName())
+            if (pkgName != null && pkgName.equals(ActivityThread.currentPackageName())
                     && userId == UserHandle.myUserId()
                     && AppPermissionUtils.shouldSpoofSelfCheck(permName))
             {
