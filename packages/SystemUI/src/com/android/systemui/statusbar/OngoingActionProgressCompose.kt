a3cd68d9614fb798471d946f7b0d6bd3b0389fdd
Pranav Vashi <neobuddy89@gmail.com>
2026-03-01 22:20:07 +0530

    SystemUI: Clean up legacy code in ongoing action progress chip
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/OngoingActionProgressCompose.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/OngoingActionProgressCompose.kt
@@ -525,12 +525,9 @@ class OnGoingActionProgressComposeController(
     init {
         Log.d(TAG, "Initializing OnGoingActionProgressComposeController")
 
-        val dummyGroup = OnGoingActionProgressGroup(null, null, null, null, null, null)
-
         try {
             javaController = OnGoingActionProgressController(
                 context,
-                dummyGroup,
                 notificationListener,
                 keyguardStateController,
                 headsUpManager,
