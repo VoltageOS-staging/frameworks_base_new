cba17fb959a974dd673981f1c43cb93488d934b6
TheHardGamer <gamelovr695@gmail.com>
2026-06-29 23:03:31 +0000

    [1/4] Introduce a native backup/restore solution
    Co-authored-by: Dmitrii bankersenator@gmail.com
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -6957,6 +6957,8 @@ public abstract class Context {
      */
     public static final String APP_LOCK_SERVICE = "app_lock";
 
+    public static final String APP_DATA_BACKUP_SERVICE = "app_data_backup";
+
     /**
      * Use with {@link #getSystemService(String)} to retrieve a
      * {@link android.app.LocaleManager}.
