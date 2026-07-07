arpokrat/arpokrat_frameworks_base

SystemUI: remove end session button on lockscreen

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -797,7 +797,8 @@ public class KeyguardIndicationController {
     }
 
     private void updateLockScreenLogoutView() {
-        if (mUserLogoutInteractor.isLogoutEnabled().getValue()) {
+        // disable lock screen end session button
+        if (false && mUserLogoutInteractor.isLogoutEnabled().getValue()) {
             mRotateTextViewController.updateIndication(
                     INDICATION_TYPE_LOGOUT,
                     new KeyguardIndication.Builder()

VoltageOS-staging/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -859,7 +859,8 @@ public class KeyguardIndicationController {
     }
 
     private void updateLockScreenLogoutView() {
-        if (mUserLogoutInteractor.isPolicyManagerLogoutEnabled().getValue()) {
+        // disable lock screen end session button
+        if (false && mUserLogoutInteractor.isPolicyManagerLogoutEnabled().getValue()) {
             mRotateTextViewController.updateIndication(
                     INDICATION_TYPE_LOGOUT,
                     new KeyguardIndication.Builder()