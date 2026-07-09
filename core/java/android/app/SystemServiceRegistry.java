DerpFest-CAF/platform_frameworks_base

SystemUI: Implement DisplayResolutionManager [1/2]
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
[nurkeinneid: move to fwb and drop sdk extensions/dependencies]
Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

core/java/android/app/SystemServiceRegistry.java
@@ -292,6 +292,8 @@ import com.android.internal.util.Preconditions;
 
 import org.derpfest.display.IRefreshRateManagerService;
 import org.derpfest.display.RefreshRateManager;
+import org.derpfest.view.DisplayResolutionManager;
+import org.derpfest.view.IDisplayResolutionManagerService;
 
 import java.time.InstantSource;
 import java.util.HashMap;
@@ -1826,6 +1828,15 @@ public final class SystemServiceRegistry {
                         return new IntrusionDetectionManager(service);
                     }
                 });
+
+        registerService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE, DisplayResolutionManager.class,
+                new CachedServiceFetcher<DisplayResolutionManager>() {
+            @Override
+            public DisplayResolutionManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE);
+                IDisplayResolutionManagerService service = IDisplayResolutionManagerService.Stub.asInterface(binder);
+                return new DisplayResolutionManager(ctx.getOuterContext(), service);
+            }});
 
         registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
                 new CachedServiceFetcher<RefreshRateManager>() {

core/java/org/derpfest/view/DisplayResolutionManager.java
@@ -1,0 +1,116 @@
+/*
+ * Copyright (C) 2022-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.view;
+
+import android.annotation.SystemService;
+import android.content.Context;
+import android.graphics.Point;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.util.Slog;
+
+/** @hide */
+@SystemService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE)
+public class DisplayResolutionManager {
+
+    private static final String TAG = "DisplayResolutionManager";
+
+    public static final int TYPE_DISABLED = 0;
+    public static final int TYPE_AOSP_FHD_DEFAULT = 1;
+    public static final int TYPE_AOSP_QHD_DEFAULT = 2;
+    public static final int TYPE_FORCED = 3;
+
+    private static final int DEVICE_TYPE =
+            SystemProperties.getInt("ro.derpfest.display.resolution_switch", TYPE_DISABLED);
+
+    public static final boolean RESTART_SYSTEMUI_ON_SWITCH =
+            SystemProperties.getBoolean("persist.sys.derpfest.resolution.restart_sysui", false);
+
+    public static final int FHD_WIDTH = 1080;
+    public static final int QHD_WIDTH = 1440;
+    public static final float SCALE = (float) FHD_WIDTH / QHD_WIDTH;
+
+    private final Context mContext;
+    private final IDisplayResolutionManagerService mService;
+
+    public DisplayResolutionManager(Context context, IDisplayResolutionManagerService service) {
+        mContext = context;
+        mService = service;
+    }
+
+    /** @hide */
+    public Point getDisplayResolution() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get display resolution. Service is null");
+            return new Point(-1, -1);
+        }
+        try {
+            return mService.getDisplayResolution();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
+    public void setDisplayResolution(int width) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to set display resolution. Service is null");
+            return;
+        }
+        if (width <= 0) {
+            Slog.e(TAG, "Failed to set display resolution. Invalid width");
+            return;
+        }
+        try {
+            mService.setDisplayResolution(width);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
+    public boolean registerDisplayResolutionListener(IDisplayResolutionListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to register resolution listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.registerDisplayResolutionListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
+    public boolean unregisterDisplayResolutionListener(IDisplayResolutionListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to unregister resolution listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.unregisterDisplayResolutionListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public static int getDeviceType() {
+        return DEVICE_TYPE >= TYPE_DISABLED &&
+                DEVICE_TYPE <= TYPE_FORCED ?
+                DEVICE_TYPE : TYPE_DISABLED;
+    }
+
+    public static float getDensityScale(int currentWidth) {
+        if (DEVICE_TYPE != TYPE_AOSP_FHD_DEFAULT && DEVICE_TYPE != TYPE_AOSP_QHD_DEFAULT) {
+            return 1f;
+        }
+        return (float) currentWidth / FHD_WIDTH;
+    }
+
+    public static boolean isDisplayWidthStrValid(String width) {
+        return "1080".equals(width) || "1440".equals(width);
+    }
+}

core/java/org/derpfest/view/IDisplayResolutionListener.aidl
@@ -1,0 +1,12 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.view;
+
+/** @hide */
+oneway interface IDisplayResolutionListener {
+
+    void onDisplayResolutionChanged(int width, int height);
+}

core/java/org/derpfest/view/IDisplayResolutionManagerService.aidl
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.view;
+
+import android.graphics.Point;
+
+import org.derpfest.view.IDisplayResolutionListener;
+
+/** @hide */
+interface IDisplayResolutionManagerService {
+
+    /* Get display resolution as Point */
+    Point getDisplayResolution();
+
+    /* Set display resolution by specific display width */
+    void setDisplayResolution(int width);
+
+    /* Register listener to listen display resolution change */
+    boolean registerDisplayResolutionListener(in IDisplayResolutionListener listener);
+
+    /* Unregister listener to listen display resolution change */
+    boolean unregisterDisplayResolutionListener(in IDisplayResolutionListener listener);
+}

services/core/java/com/android/server/wm/WindowManagerServiceExt.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.wm;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.server.DisplayResolutionController;
+import com.android.server.DisplayThread;
+
+import org.derpfest.view.DisplayResolutionManager;
+
+class WindowManagerServiceExt {
+
+    private static class InstanceHolder {
+        private static final WindowManagerServiceExt INSTANCE = new WindowManagerServiceExt();
+    }
+
+    static WindowManagerServiceExt getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private WindowManagerService mWms;
+
+    void init(WindowManagerService wms) {
+        mWms = wms;
+        TopActivityRecorder.getInstance().initWms(wms);
+        DisplayResolutionController.getInstance().init(wms.mContext, wms);
+    }
+
+    void systemReady() {
+        DisplayResolutionController.getInstance().systemReady();
+    }
+
+    void registerContentObserver(ContentObserver observer) {
+        final ContentResolver cr = mWms.mContext.getContentResolver();
+    }
+
+    void loadSettings() {
+    }
+
+    boolean onSettingsChanged(Uri uri) {
+        return false;
+    }
+
+    void onUserSwitched() {
+        loadSettings();
+    }
+
+    int getDensityWithScale(int density) {
+        final int width = DisplayResolutionController.getInstance().getResolution().x;
+        if (width > 0) {
+            return (int) (density * DisplayResolutionManager.getDensityScale(width));
+        }
+        return density;
+    }
+}

VoltageOS-staging/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

core/java/android/app/SystemServiceRegistry.java
@@ -341,6 +341,8 @@ import com.android.modules.utils.ravenwood.RavenwoodHelper;
 
 import com.voltage.display.IRefreshRateManagerService;
 import com.voltage.display.RefreshRateManager;
+import com.voltage.view.DisplayResolutionManager;
+import com.voltage.view.IDisplayResolutionManagerService;
 
 import java.util.Map;
 import java.util.Objects;
@@ -2187,6 +2189,15 @@ public final class SystemServiceRegistry {
                         }
                     });
         }
+
+        registerService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE, DisplayResolutionManager.class,
+                new CachedServiceFetcher<DisplayResolutionManager>() {
+            @Override
+            public DisplayResolutionManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE);
+                IDisplayResolutionManagerService service = IDisplayResolutionManagerService.Stub.asInterface(binder);
+                return new DisplayResolutionManager(ctx.getOuterContext(), service);
+            }});
 
         registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
                 new CachedServiceFetcher<RefreshRateManager>() {

core/java/com/voltage/view/DisplayResolutionManager.java
@@ -1,0 +1,116 @@
+/*
+ * Copyright (C) 2022-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.view;
+
+import android.annotation.SystemService;
+import android.content.Context;
+import android.graphics.Point;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.util.Slog;
+
+/** @hide */
+@SystemService(Context.DISPLAY_RESOLUTION_MANAGER_SERVICE)
+public class DisplayResolutionManager {
+
+    private static final String TAG = "DisplayResolutionManager";
+
+    public static final int TYPE_DISABLED = 0;
+    public static final int TYPE_AOSP_FHD_DEFAULT = 1;
+    public static final int TYPE_AOSP_QHD_DEFAULT = 2;
+    public static final int TYPE_FORCED = 3;
+
+    private static final int DEVICE_TYPE =
+            SystemProperties.getInt("ro.voltage.display.resolution_switch", TYPE_DISABLED);
+
+    public static final boolean RESTART_SYSTEMUI_ON_SWITCH =
+            SystemProperties.getBoolean("persist.sys.voltage.resolution.restart_sysui", false);
+
+    public static final int FHD_WIDTH = 1080;
+    public static final int QHD_WIDTH = 1440;
+    public static final float SCALE = (float) FHD_WIDTH / QHD_WIDTH;
+
+    private final Context mContext;
+    private final IDisplayResolutionManagerService mService;
+
+    public DisplayResolutionManager(Context context, IDisplayResolutionManagerService service) {
+        mContext = context;
+        mService = service;
+    }
+
+    /** @hide */
+    public Point getDisplayResolution() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get display resolution. Service is null");
+            return new Point(-1, -1);
+        }
+        try {
+            return mService.getDisplayResolution();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
+    public void setDisplayResolution(int width) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to set display resolution. Service is null");
+            return;
+        }
+        if (width <= 0) {
+            Slog.e(TAG, "Failed to set display resolution. Invalid width");
+            return;
+        }
+        try {
+            mService.setDisplayResolution(width);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
+    public boolean registerDisplayResolutionListener(IDisplayResolutionListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to register resolution listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.registerDisplayResolutionListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
+    public boolean unregisterDisplayResolutionListener(IDisplayResolutionListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to unregister resolution listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.unregisterDisplayResolutionListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public static int getDeviceType() {
+        return DEVICE_TYPE >= TYPE_DISABLED &&
+                DEVICE_TYPE <= TYPE_FORCED ?
+                DEVICE_TYPE : TYPE_DISABLED;
+    }
+
+    public static float getDensityScale(int currentWidth) {
+        if (DEVICE_TYPE != TYPE_AOSP_FHD_DEFAULT && DEVICE_TYPE != TYPE_AOSP_QHD_DEFAULT) {
+            return 1f;
+        }
+        return (float) currentWidth / FHD_WIDTH;
+    }
+
+    public static boolean isDisplayWidthStrValid(String width) {
+        return "1080".equals(width) || "1440".equals(width);
+    }
+}

core/java/com/voltage/view/IDisplayResolutionListener.aidl
@@ -1,0 +1,12 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.view;
+
+/** @hide */
+oneway interface IDisplayResolutionListener {
+
+    void onDisplayResolutionChanged(int width, int height);
+}

core/java/com/voltage/view/IDisplayResolutionManagerService.aidl
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.view;
+
+import android.graphics.Point;
+
+import com.voltage.view.IDisplayResolutionListener;
+
+/** @hide */
+interface IDisplayResolutionManagerService {
+
+    /* Get display resolution as Point */
+    Point getDisplayResolution();
+
+    /* Set display resolution by specific display width */
+    void setDisplayResolution(int width);
+
+    /* Register listener to listen display resolution change */
+    boolean registerDisplayResolutionListener(in IDisplayResolutionListener listener);
+
+    /* Unregister listener to listen display resolution change */
+    boolean unregisterDisplayResolutionListener(in IDisplayResolutionListener listener);
+}

services/core/java/com/android/server/wm/WindowManagerServiceExt.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.wm;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.server.DisplayResolutionController;
+import com.android.server.DisplayThread;
+
+import com.voltage.view.DisplayResolutionManager;
+
+class WindowManagerServiceExt {
+
+    private static class InstanceHolder {
+        private static final WindowManagerServiceExt INSTANCE = new WindowManagerServiceExt();
+    }
+
+    static WindowManagerServiceExt getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private WindowManagerService mWms;
+
+    void init(WindowManagerService wms) {
+        mWms = wms;
+        TopActivityRecorder.getInstance().initWms(wms);
+        DisplayResolutionController.getInstance().init(wms.mContext, wms);
+    }
+
+    void systemReady() {
+        DisplayResolutionController.getInstance().systemReady();
+    }
+
+    void registerContentObserver(ContentObserver observer) {
+        final ContentResolver cr = mWms.mContext.getContentResolver();
+    }
+
+    void loadSettings() {
+    }
+
+    boolean onSettingsChanged(Uri uri) {
+        return false;
+    }
+
+    void onUserSwitched() {
+        loadSettings();
+    }
+
+    int getDensityWithScale(int density) {
+        final int width = DisplayResolutionController.getInstance().getResolution().x;
+        if (width > 0) {
+            return (int) (density * DisplayResolutionManager.getDensityScale(width));
+        }
+        return density;
+    }
+}